cce765ef90126f1219a3e234da50be762b0cc124
mahmoud <mahmoud.elyamany.official@gmail.com>
2025-08-11 18:18:25 +0300

    editing mobile version related imports and configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@app/App";
-import { AppProviders } from "@app/providers";
+import App from "../src/app/App";
+import { AppProviders } from "../src/app/providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/native";
 import { BookingList } from "@features/booking/components/BookingList";
 
-const Wrap = styled.div`
-  max-width: 880px; margin: 0 auto;
+const Wrap = styled.View`
+  flex: 1;
+  padding: 24px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 16px;
 `;
 
 export const Home: React.FC = () => (
   <Wrap>
-    <h2>Home</h2>
+    <Title>Home</Title>
     <BookingList />
   </Wrap>
 );
